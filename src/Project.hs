36153dd7e2e498b4c19938793a41d07bb45a4165
Connor Redfern <connor.redfern@optusnet.com.au>
2023-07-31 07:47:35 +0800

    Parers & initial data-structures

--- src/Project.hs
+++ src/Project.hs
@@ -1,11 +1,50 @@
-module Project () where
+{-# OPTIONS_GHC -Wno-orphans #-}
+module Project
+  ( ProjectFile(..)
+  , BuildFile(..)
+  , project_file
+  , run_parser) where
+
+import Data.Text (Text, pack, unpack)
+import qualified Text.Megaparsec as Megaparsec
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Char
+import Text.Megaparsec hiding (runParser)
+import Prettyprinter
+
+type Parser = Parsec Text Text
+
+sc :: Parser () 
+sc = L.space
+  space1
+  (L.skipLineComment "#")   
+  (L.skipBlockComment "(#" "#)")
+
+lexeme :: Parser a -> Parser a  
+lexeme = L.lexeme sc
+
+symbol :: Parser Text  
+symbol = pack <$> some (satisfy (\v -> v /= '#' && v /= '(' && v /= ')')) 
+
+data ProjectFile = ProjectFile { version :: (Int, Int, Int) }
+
+project_file :: Parser ProjectFile
+project_file = do
+  sc
+  vnum <- between (lexeme $ char '(') (lexeme $ char ')') version_num
+  sc 
+  pure $ ProjectFile vnum
+  where 
+    version_num :: Parser (Int, Int, Int)
+    version_num = do
+      s <- symbol
+      if (s == "glaze-version") then
+        lexeme $ (,,) <$> L.decimal <*> L.decimal <*> L.decimal
+      else
+        fail ("Expecting glaze-version, got: " <> unpack s)
 
 
-data ProjectFile = ProjectFile Version
-
-
-process_project_file :: String -> ProjectFile
-
+  
 data BuildFile
   = DocumentBulid
    { name :: Text
@@ -19,4 +58,15 @@ data BuildFile
    , subcomponents :: [Text] -- libraries
    }
 
-process_build_file :: String -> 
+-- process_build_file :: Parser BuildFile
+-- process_build_file = do 
+  
+
+
+run_parser :: Parser a -> Text -> Text -> Either (Doc ann) a
+run_parser p file input = case Megaparsec.runParser p (unpack file) input of
+  Left err -> Left $ pretty $ errorBundlePretty err
+  Right val -> Right val
+
+instance ShowErrorComponent Text where 
+  showErrorComponent = unpack
